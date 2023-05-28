741e292254df005d45ed641ede3f0e40bbafb25e
anzz1 <anzz1@live.com>
2023-05-29 01:28:29 +0300

    ota: small tweaks

--- extras/Tools/OTA Update.pak/launch.sh
+++ extras/Tools/OTA Update.pak/launch.sh
@@ -16,15 +16,15 @@ rm -f update-extras.zip
 
 if [ -z "$(curl --version 2> /dev/null)" ] || [ -z "$(miniunz 2> /dev/null)" ]; then
 	show okay.png
-	say "A system library is missing!"$'\n'"Please reinstall DotUI."
-	confirm any
+	say "A system library is missing!"$'\n'$'\n'"Please reinstall DotUI."$'\n'
+	confirm only
 	exit 0
 fi
 
 if ! (ip route get 1.1.1.1); then
 	show okay.png
 	say "No internet connection."
-	confirm any
+	confirm only
 	exit 0
 fi
 
@@ -34,21 +34,22 @@ TAG="$(curl --silent -f -m 30 -L -k "https://api.github.com/repos/anzz1/DotUI-X/
 if [ -z "$TAG" ]; then
 	show okay.png
 	say "Failed to check for updates."
-	confirm any
+	confirm only
 	exit 0
 fi
 if [ "$MY_VERSION" = "$TAG" ]; then
 	show okay.png
 	say "You have the latest version !"
-	confirm any
+	confirm only
 	exit 0
 fi
 show confirm.png
-say "Update is available !"$'\n\n'"Yours: "$MY_VERSION""$'\n'"Latest: "$TAG""$'\n\n'"Do you want to update?"
+say "Update is available !"$'\n\n'"Yours: "$MY_VERSION""$'\n'"Latest: "$TAG""$'\n\n'"Do you want to update?"$'\n'
 if ! confirm; then exit 0; fi
 
 progressui &
 PROG_PID=$!
+sleep 0.5
 
 progress 0 "Checking for free space ..."
 
@@ -57,8 +58,8 @@ if ! [ "$(df -m /mnt/SDCARD/ | tail -1 | awk '{print $(NF-2);exit}')" -gt 100 ] 
 	progress quit
 	wait $PROG_PID
 	show okay.png
-	say "Not enough free space!"$'\n'"100MB of free space is"$'\n'"required for OTA update."
-	confirm any
+	say "Not enough free space !"$'\n'$'\n'"100MB of free space is"$'\n'"required for OTA update."$'\n'
+	confirm only
 	exit 0
 fi
 
@@ -76,7 +77,7 @@ if [ $? -ne 0 ] || [ ! -f update-base.zip ] || [ ! -f update-extras.zip ]; then
 	wait $PROG_PID
 	show okay.png
 	say "Update download failed."
-	confirm any
+	confirm only
 	exit 0
 fi
 
@@ -98,7 +99,7 @@ if [ $? -ne 0 ]; then
 	wait $PROG_PID
 	show okay.png
 	say "Update extraction failed."
-	confirm any
+	confirm only
 	exit 0
 fi
 
